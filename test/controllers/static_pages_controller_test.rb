9a582113def188d3e65e7ea94404d8a5d6bf0f4a
kekosh2 <kekosh2@example.com>
2019-12-28 05:18:36 +0000

    tutorial 3.4.2 演習前まで

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,20 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  #homeページのテスト
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
-
+  
+  #helpページのテスト
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
   
   #aboutページのテスト
-  test "should get about" do
+  test "shoud get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
 end
