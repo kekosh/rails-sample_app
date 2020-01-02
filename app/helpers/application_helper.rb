5c2e5cea15b066d27922bb10bcabd403bf828a79
kekosh2 <kekosh2@example.com>
2020-01-02 09:46:30 +0000

    tutorial 4.1.2add comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,8 @@ module ApplicationHelper
     #ページごとの完全なタイトルを返す
     def full_title(page_title="")
         base_title = "Ruby on Rails Tutorial Sample App"
+        
+        #erbからの呼び出し時にprovideによるタイトル設定がなかった場合
         if page_title.empty?
             base_title
         else
