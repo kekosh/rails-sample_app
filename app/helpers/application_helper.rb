4feafb15effbc625baf89825af75d224ffca90b1
kekosh2 <kekosh2@example.com>
2020-01-03 10:14:02 +0000

    tutorial 4.3.1 fin.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
     
+    #rubyでは、他のモジュールを読み込んで利用する場合に"include xxxx"として
+    #モジュールを読み込むことができるが、railsでは自動的に「Helper」モジュールを
+    #読み込むため、別途includeする必要はない。(=>module ApplicationHelperの効果)
+    
     #ページごとの完全なタイトルを返す
     def full_title(page_title="")
         base_title = "Ruby on Rails Tutorial Sample App"
